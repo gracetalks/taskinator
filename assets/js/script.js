52747f8b8efed35c6cc6b7b5d1dc8e29d25ab467
Cynthia <cynthia@thegracetalks.com>
2021-05-12 18:33:44 -0500

    adding the ability to add tasks via script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 //Assign button element object representation to a variable
 var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
+
+buttonEl.createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
