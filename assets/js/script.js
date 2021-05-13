b0cb858f35b196fbdfb3ba7cac16245539538ae7
Cynthia <cynthia@thegracetalks.com>
2021-05-12 20:16:18 -0500

    adding content to script.js to change TaskHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 //Assign button element object representation to a variable
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+   event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  };
 
-buttonEl.createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-};
+//buttonEl.createTaskHandler = function() {
+    //var listItemEl = document.createElement("li");
+    //listItemEl.className = "task-item";
+    //listItemEl.textContent = "This is a new task.";
+    //tasksToDoEl.appendChild(listItemEl);
+//};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
